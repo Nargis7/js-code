1039373c3cf2ea991c9af7c70bd910ef1e67b488
Nargis Perween <ronaldonargis786@gmail.com>
2025-03-15 15:44:43 +0000

    datatypes file

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello") we are using node js, not browser
+
+console.log(3+3);
+let name = "nargis"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number=> 2 to power 53
+// bigint
+// string=>""
+// boolean => true or false
+// null => standalone value
+// undefined =>
+//  symbol => unique
+
+// object
+
+// null is a object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
