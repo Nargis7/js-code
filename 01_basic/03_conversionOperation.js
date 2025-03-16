e82b3b6fb1613a46c4773ee09f7a7734da86ba01
Nargis Perween <ronaldonargis786@gmail.com>
2025-03-16 07:02:39 +0000

    conversion file

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -19,11 +19,49 @@ let valueInNumber = Number(score)
 let isLoggedIn = "nargis" //true
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *************************Operations************************************
+
+let value =3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2); //4
+// console.log(2-2); //0
+// console.log(2*2);//4
+// console.log(2**2); //4
+// console.log(2/2); //1
+// console.log(2%2); //0
+
+// let str1 ="hello"
+// let str2 = "nargis"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true); //1
+// console.log(+ ""); //0
+
+let num1 , num2 , num3
+num1  = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// prefix and postfix js mdn
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
 
 
