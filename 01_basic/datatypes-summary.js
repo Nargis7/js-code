957393142046ab84e0a96b8e9480260769d35a1d
Nargis Perween <ronaldonargis786@gmail.com>
2025-03-25 05:10:09 +0000

    add files

--- 01_basic/datatypes-summary.js
+++ 01_basic/datatypes-summary.js
@@ -73,3 +73,18 @@ function greet(name) { return "Hello, " + name + "!"; }
 console.log(greet("Ankit"));
 
 console.log(typeof greet)
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// stack(primitive) , heap(non primitive)
+let n1 = 10;
+let n2 = n1;
+n2 = 20;
+console.log(n1); // 10 (remains unchanged)
+// Here, n1 and n2 are stored in the stack, and n2 gets a copy of a’s value.
+
+let obj1 = { name: "Ajay" };
+let obj2 = obj1;
+obj2.name = "Vijay";
+console.log(obj1.name); // "Vijay"
+// Here, obj1 and obj2 reference the same memory location in the heap. // original value
