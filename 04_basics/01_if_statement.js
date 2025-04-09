52134cd3b4120b8723666ac9564e7eb688002efe
Nargis Perween <ronaldonargis786@gmail.com>
2025-04-09 06:32:29 +0000

    added conditonal op

---
+++ 04_basics/01_if_statement.js
@@ -0,0 +1,7 @@
+let age = 17
+if(age>= 18){
+    console.log("you can drive");
+}
+if(age<18){
+    console.log("you cannot drive");
+}
---
+++ 04_basics/02_else.js
@@ -0,0 +1,15 @@
+// let num = 11;
+// if(num%2==0){
+//     console.log("even");
+// }else{
+//     console.log("odd");
+// }
+
+let mode = "Dark";
+let color;
+if(mode=="Dark") {
+    color = "Black";
+}else{
+    color = "White";
+}
+console.log(color);
